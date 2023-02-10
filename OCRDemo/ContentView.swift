5c988c6224815559644fcf3abe7e06d39bde6d97
Sam Winslow <4645779+samwinslow@users.noreply.github.com>
2023-02-10 15:27:56 -0500

    Initial Commit

---
+++ OCRDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OCRDemo
+//
+//  Created by Sam Winslow on 2023-02-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OCRDemo/OCRDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  OCRDemoApp.swift
+//  OCRDemo
+//
+//  Created by Sam Winslow on 2023-02-10.
+//
+
+import SwiftUI
+
+@main
+struct OCRDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
